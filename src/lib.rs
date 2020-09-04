36b3de5fb2e76bebe577fd4aea1aaf51f9521d88
Jeff Shen <jshen2014@hotmail.com>
2020-09-04 04:15:58 -0400

    finished macro and added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use]
+pub mod approx_eq {
+    #[macro_export]
+    macro_rules! assert_approx_eq {
+        ($x: expr, $y: expr) => {
+            let eps = 1e-6;
+            let (x, y): (f64, f64) = ($x, $y);
+            assert!((&x - &y).abs() / [x, y].iter().cloned().fold(0. / 0., f64::min) < eps)
+        };
+        ($x: expr, $y: expr, $e: expr) => {
+            let (x, y): (f64, f64) = ($x, $y);
+            assert!((&x - &y).abs() / [x, y].iter().cloned().fold(0. / 0., f64::min) < $e)
+        };
     }
+}
+
+#[test]
+fn test_noeps() {
+    assert_approx_eq!(1., 1.);
+    assert_approx_eq!(1., 1.000001);
+}
+
+#[test]
+fn test_witheps() {
+    assert_approx_eq!(1.0000000001, 1., 1e-5);
+}
+
+#[test]
+#[should_panic(expected = "assertion failed")]
+fn test_invalid() {
+    assert_approx_eq!(1.0000000001, 1., 1e-10);
 }
